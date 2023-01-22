d5bd4bb9b1e59bfcd18798bf94f82feba25afa8c
Marinakrash <66419111+marinakrash@users.noreply.github.com>
2023-01-22 19:47:00 +0200

    Update sort_files.py

--- sort_files.py
+++ sort_files.py
@@ -12,51 +12,57 @@ def collect_fileinfos(path_directory):
     music_files = []
     video_files = []
     unknown_format = []
+    new_name=''
     type=''
-    content_dir = os.listdir(path_directory)
-    if '.DS_Store' in content_dir: content_dir.remove('.DS_Store')
-    for filename in content_dir:
-        path_file = os.sep.join([path_directory, filename])
-        nor_name = normalize.normalize(filename)
-        if os.path.isdir(path_file):
-            collect_fileinfos(path_file)
-            if not os.listdir(path_file):
-                os.rmdir(path_file)
-        else:
-            filesurvey.append((path_directory, filename))
-            type = os.path.splitext(filename)[-1]
-            all_types.append(type)
-            if type in ['.jpeg', '.png', '.jpg', '.svg']:
-                directory = os.sep.join([path_directory, 'images'])
-                if not os.path.exists(directory): os.makedirs(directory)
-                shutil.move(path_file, (f'{directory}' + f'/{nor_name}'))
-                img_files.append(nor_name)
-            if type in ['.avi', '.mp4', '.mov', '.mkv']:
-                directory = os.sep.join([path_directory, 'videos'])
-                if not os.path.exists(directory): os.makedirs(directory)
-                shutil.move(path_file, (f'{directory}' + f'/{nor_name}'))
-                video_files.append(nor_name)
-            if type in ['.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx']:
-                directory = os.sep.join([path_directory, 'documents'])
-                if not os.path.exists(directory): os.makedirs(directory)
-                shutil.move(path_file, (f'{directory}' + f'/{nor_name}'))
-                doc_files.append(nor_name)
-            if type in ['.mp3', '.ogg', '.wav', '.amr']:
-                directory = os.sep.join([path_directory, 'audio'])
-                if not os.path.exists(directory): os.makedirs(directory)
-                shutil.move(path_file, (f'{directory}' + f'/{nor_name}'))
-                music_files.append(nor_name)
-            if type in ['.zip', '.gz', '.tar']:
-                directory = os.sep.join([path_directory, 'archives'])
-                if not os.path.exists(directory): os.makedirs(directory)
-                shutil.move(path_file, (f'{directory}' + f'/{nor_name}'))
-                ar_dir = f'{directory}'+f'{os.path.splitext(nor_name)[:-1]}'
-                os.makedirs(ar_dir)
-                shutil.unpack_archive(path_file, ar_dir)
-                arch_files.append(nor_name)
-            if type in ['.wenb', '.webm', '.sql', '', '.crdownload'] or type == [] or type is None:
-                shutil.move(path_file, (f'{path_directory}' + f'/{nor_name}'))
-                unknown_format.append(type)
+    main_dir=path_directory
+    def dir_work(path_directory, main_dir):
+        content_dir = os.listdir(path_directory)
+        if '.DS_Store' in content_dir: content_dir.remove('.DS_Store')
+        for filename in content_dir:
+            path_file = os.sep.join([path_directory, filename])
+            nor_name = normalize.normalize(filename)
+            if os.path.isdir(path_file):
+                dir_work(path_file, main_dir)
+                if len(os.listdir(path_file)) == 0:
+                    print(len(os.listdir(path_file)))
+                    os.rmdir(path_file)
+            else:
+                filesurvey.append((path_directory, filename))
+                type = os.path.splitext(filename)[-1]
+                all_types.append(type)
+                if type in ['.jpeg', '.png', '.jpg', '.svg']:
+                    directory = os.sep.join([main_dir, 'images'])
+                    if not os.path.exists(directory): os.makedirs(directory)
+                    shutil.move(path_file, (f'{directory}' + f'/{nor_name}'))
+                    img_files.append(nor_name)
+                if type in ['.avi', '.mp4', '.mov', '.mkv']:
+                    directory = os.sep.join([main_dir, 'videos'])
+                    if not os.path.exists(directory): os.makedirs(directory)
+                    shutil.move(path_file, (f'{directory}' + f'/{nor_name}'))
+                    video_files.append(nor_name)
+                if type in ['.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx']:
+                    directory = os.sep.join([main_dir, 'documents'])
+                    if not os.path.exists(directory): os.makedirs(directory)
+                    shutil.move(path_file, (f'{directory}' + f'/{nor_name}'))
+                    doc_files.append(nor_name)
+                if type in ['.mp3', '.ogg', '.wav', '.amr']:
+                    directory = os.sep.join([main_dir, 'audio'])
+                    if not os.path.exists(directory): os.makedirs(directory)
+                    shutil.move(path_file, (f'{directory}' + f'/{nor_name}'))
+                    music_files.append(nor_name)
+                if type in ['.zip', '.gz', '.tar']:
+                    directory = os.sep.join([main_dir, 'archives'])
+                    if not os.path.exists(directory): os.makedirs(directory)
+                    shutil.move(path_file, (f'{directory}' + f'/{nor_name}'))
+                    new_name=os.path.splitext(nor_name)[0]
+                    ar_dir = f'{directory}'+f'/{new_name}'
+                    if not os.path.exists(ar_dir): os.makedirs(ar_dir)
+                    shutil.unpack_archive((f'{directory}' + f'/{nor_name}'), ar_dir)
+                    arch_files.append(nor_name)
+                if type in ['.wenb', '.webm', '.sql', '', '.crdownload'] or type == [] or type is None:
+                    shutil.move(path_file, (f'{main_dir}' + f'/{nor_name}'))
+                    unknown_format.append(type)
+    dir_work(path_directory, main_dir)
     print(f'arch_files {arch_files}')
     print(f'doc_files {doc_files}')
     print(f'img_files {img_files}')
